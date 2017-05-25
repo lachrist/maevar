dc651dbc02dbba4f32f744ba66e2580b5e2a3923
lachrist <lachrist@vub.ac.be>
2017-05-25 17:38:45 +0200

    removed read dependency, using stdin.setRawMode(true) instead

--- bin.js
+++ bin.js
@@ -1,50 +1,72 @@
 #!/usr/bin/env node
 
 var Fs = require("fs");
-var Path = require("path");
+var Stream = require("stream");
 var ChildProcess = require("child_process");
-var Read = require("read");
 
-if (process.argv.length !== 3) {
-  process.stderr.write("Usage: node.js edit.js path/to/encrypted\n");
+var END_OF_TEXT = Buffer.from("\u0003", "utf8");
+var END_OF_FILE = Buffer.from("\u0004", "utf8");
+var BACKSPACE = Buffer.from("\u0008", "utf8");
+var LINE_FEED = Buffer.from("\u000a", "utf8");
+var CARRIAGE_RETURN = Buffer.from("\u000d", "utf8");
+var DELETE = Buffer.from("\u007f", "utf8");
+
+function exit (message) {
+  process.stderr.write(message+"\n");
   process.exit(1);
 }
 
+if (process.argv.length !== 3)
+  exit("Usage: maevar path/to/encrypted/file");
+if (!ChildProcess.spawnSync("which", ["vim"]).stdout.toString())
+  exit("vim not found, download at: http://www.vim.org/download.php");
+if (!ChildProcess.spawnSync("which", ["srm"]).stdout.toString())
+  exit("srm not found, download at: https://sourceforge.net/projects/srm/");
+
 var encrypted = process.argv[2];
-var decrypted = Path.dirname(encrypted)+"/tmp.txt";
+var decrypted = encrypted+Math.random().toString(36).substring(2);
 
 function check (error) {
   if (error) {
-    ChildProcess.spawnSync("srm", [decrypted], {stdio:"inherit"});
-    throw error;
+    ChildProcess.spawnSync("srm", [decrypted], {stdio:"ignore"});
+    exit(String(error));
   }
 }
 
-function execute (command, arguments) {
-  var result = ChildProcess.spawnSync(command, arguments, {
-    stdio: ["ignore", "ignore", "pipe"],
-    encoding: "utf8"
+function prompt (text, limit, callback) {
+  if (process.stdin.isRaw)
+    check("Concurrent prompt, this should never happen...");
+  var input = "";
+  function done () {
+    process.stdin.setRawMode(false);
+    process.stdin.pause();
+    process.stdin.removeAllListeners("data");
+    process.stdout.write("\n");
+    callback(input);
+  }
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+  process.stdin.on("data", function(key) {
+    if (!key.compare(END_OF_TEXT) || !key.compare(END_OF_FILE))
+      check("Interrupted by the user");
+    if (!key.compare(BACKSPACE) || !key.compare(DELETE))
+      return input = input.substring(0, input.length - 1);
+    if (!key.compare(LINE_FEED) || !key.compare(CARRIAGE_RETURN))
+      return done();
+    input += key.toString("utf8");
+    (input.length >= limit) && done();
   });
-  check(result.error || (result.status !== 0 && result.stderr));
-
+  process.stdout.write(text);
 }
 
-function edit () { ChildProcess.spawnSync("vim", [decrypted], {stdio:"inherit"}) }
+process.on("SIGINT", function () { check("Interrupted by the user") });
 
-function encrypt (password) {
-  execute("openssl", [
-    "enc",
-    "-aes256",
-    "-base64",
-    "-k", password,
-    "-in", decrypted,
-    "-out", encrypted
-  ]);
-  execute("srm", [decrypted]);
+function errorify (result) {
+  return result.error || (result.status ? result.status+" "+result.stderr : null);
 }
 
 function decrypt (password) {
-  execute("openssl", [
+  check(errorify(ChildProcess.spawnSync("openssl", [
     "enc",
     "-d",
     "-aes256",
@@ -52,35 +74,48 @@ function decrypt (password) {
     "-k", password,
     "-in", encrypted,
     "-out", decrypted
-  ]);
+  ])));
+}
+
+function edit () {
+  check(errorify(ChildProcess.spawnSync("vim", ["-n", decrypted], {stdio:"inherit"})));
+}
+
+function encrypt (password) {
+  check(errorify(ChildProcess.spawnSync("openssl", [
+    "enc",
+    "-aes256",
+    "-base64",
+    "-k", password,
+    "-in", decrypted,
+    "-out", encrypted
+  ])));
+  check(errorify(ChildProcess.spawnSync("srm", [decrypted])));
 }
 
 function newpassword (callback) {
-  Read({prompt:"New password...", silent:true}, function (error, password) {
-    check(error);
-    if (password.length >= 4)
-      return Read({prompt:"Confirm...", silent:true}, function (error, confirm) {
-        check(error);
-        if (confirm === password)
-          return callback(password);
-        process.stdout.write("Password mismatch, please retype.\n")
-        ask(callback);
-      });
-    process.stdout.write("Password too short, please retype.\n");
-    newpassword(callback);
+  prompt("New password: ", Infinity, function (password) {
+    prompt("Confirm: ", Infinity, function (confirm) {
+      if (password === confirm)
+        return callback(password);
+      process.stdout.write("Password mismatch, please retry...\n");
+      newpassword(callback);
+    });
   });
 }
 
 Fs.stat(encrypted, function (error, stats) {
-  if (error && error.code === "ENOENT")
-    return (edit(), newpassword(encrypt));
-  check(error || (stats.isFile() ? null : new Error(encrypted+" is not a file")));
-  Read({prompt:"Password...", silent:true}, function (error, password) {
-    check(error);
-    decrypt(password);
-    edit();
-    Read({prompt: "New password? [y/n]"}, function (error, answer) {
-      (answer === "y") ? newpassword(encrypt) : encrypt(password);
+  if (error && error.code === "ENOENT") {
+    edit()
+    newpassword(encrypt);
+  } else {
+    check(error || (stats.isFile() ? null : encrypted+" is not a file"));
+    prompt("Password: ", Infinity, function (password) {
+      decrypt(password);
+      edit();
+      prompt("New password? [y/n]", 1, function (answer) {
+        (answer === "y") ? newpassword(encrypt) : encrypt(password);
+      });
     });
-  });
+  }
 });
